19cac6406eb8051697f1c412b6e8972c6c7e7843
HAN SIMENG <hans0035@e.ntu.edu.sg>
2019-10-18 20:19:06 +0800

    update

---
+++ Part_A/expriments/plot_correlation_mat.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("../data/admission_predict.csv")
+
+USED_data = data[['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR', 'CGPA', 'Research', 'Chance of Admit']]
+
+print(USED_data.head())
+corr = USED_data.corr()
+ax = sns.heatmap(
+    corr,
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    square=True
+)
+
+ax.set_xticklabels(
+    ax.get_xticklabels(),
+    rotation=45,
+    horizontalalignment='right'
+)
+
+plt.show()
