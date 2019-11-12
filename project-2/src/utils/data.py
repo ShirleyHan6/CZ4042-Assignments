e509e0a5c50061f0a729874776c3bfad5017834b
HAN SIMENG <hans0035@e.ntu.edu.sg>
2019-11-12 11:58:43 +0800

    text train skeleton

---
+++ project-2/src/utils/data.py
@@ -0,0 +1,42 @@
+import numpy as np
+from torch.utils import data as tdata
+
+
+def split_test_data(dataset: tdata.Dataset, test_ratio: float = 0.3):
+    num_instances = len(dataset)
+    test_size = int(num_instances * test_ratio)
+    train_size = num_instances - test_size
+    train_data, test_data = tdata.random_split(dataset, (train_size, test_size))
+    return train_data, test_data
+
+
+def get_indices(n_splits, n):
+    """
+    Indices of the set test
+    Args:
+        n_splits: folds number
+        n: number of fold size
+    """
+    partitions = np.ones(n_splits) * int(n / n_splits)
+    partitions[0:(n % n_splits)] += 1
+    indices = np.arange(n).astype(int)
+    current = 0
+    for fold_size in partitions:
+        start = current
+        stop = current + fold_size
+        current = stop
+        yield (indices[int(start):int(stop)])
+
+
+def k_folds(data: tdata.Dataset, n_splits: int):
+    """
+    Generates folds for cross validation
+    Args:
+        data:
+        n_splits: folds number
+    """
+    n = len(data)
+    indices = np.arange(n).astype(int)
+    for val_idx in get_indices(n_splits, n):
+        train_idx = np.setdiff1d(indices, val_idx)
+        yield tdata.Subset(data, train_idx), tdata.Subset(data, val_idx)
---
+++ project-2/src/utils/utils.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+
+def avg_list(alist):
+    return sum(alist) / len(alist)
+
+
+def get_accuracy(scores: torch.Tensor, labels: torch.Tensor):
+    predicted_labels = scores.argmax(dim=1)
+    indicator = (predicted_labels == labels)
+    num_matches = indicator.sum()
+
+    return num_matches.float()
+
+
+def init_weight(m):
+    """
+    General weight initializer from https://gist.github.com/jeasinema/ed9236ce743c8efaf30fa2ff732749f5
+    Args:
+        :param m:
+    Usage:
+        model = Model()
+        model.apply(weight_init)
+    """
+    if isinstance(m, nn.Conv1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm3d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.Linear):
+        init.xavier_normal_(m.weight.data)
+        init.normal_(m.bias.data)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.LSTMCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRUCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+
+
+def plot_train_val_accuracies(train_accs, val_accs):
+    epochs = np.arange(1, len(val_accs) + 1)
+    fig = plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    ax.plot(epochs, train_accs, label='train accuracy')
+    ax.plot(epochs, val_accs, label='test accuracy')
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.title('Train and test accuracy against epoch')
+    plt.show()
+
+
+def plot_train_val_loss(train_loss, val_loss):
+    epochs = np.arange(1, len(val_loss) + 1)
+    fig = plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    ax.plot(epochs, train_loss, label='train loss')
+    ax.plot(epochs, val_loss, label='test loss')
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.yscale('log')
+    plt.title('Train and test accuracy against epoch')
+    plt.show()
+
+
+def plot_batched_accuracies(acc_dict: dict, *, title: str = 'Validation accuracy against epoch', label_base: str = '',
+                            yscale: str = 'linear'):
+    keys, val_accs = list(acc_dict.keys()), list(acc_dict.values())
+
+    orders = np.arange(1, len(val_accs[0]) + 1)
+
+    # plot val_accs against epochs
+    fig = plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    for i in range(len(val_accs)):
+        ax.plot(orders, val_accs[i], label=(label_base + str(keys[i])))
+        ax.legend(bbox_to_anchor=(0.9, 1), loc='upper left', borderaxespad=0.)
+    plt.yscale(yscale)
+    plt.title(title)
+    plt.show()
